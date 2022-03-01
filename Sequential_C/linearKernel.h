8cf280bae3d967451f14c6a7d7f2135cd0d9d987
Zavier-opt <77583744+Zavier-opt@users.noreply.github.com>
2022-03-01 15:25:19 -0500

    add linear kernel function

---
+++ Sequential_C/linearKernel.h
@@ -0,0 +1,13 @@
+void linearKernel(float* x, float* y, float b, float* res) {
+	/* x is a*n; y is b*n; b is a scalar; res is a*b */
+	int a = sizeof(x) / sizeof(float);
+	int n = sizeof(x[0]) / sizeof(float);
+	int b = sizeof(y) / sizeof(float);
+	for (int i = 0; i < a; i++) {
+		for (int j = 0; i < b; j++) {
+			for (int k = 0; k < n; k++) {
+				res[i][j] += x[i][k] * y[j][k];
+			}
+		}
+	}
+}
