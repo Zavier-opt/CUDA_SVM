74ced71b651e359f578d3169300927972d710557
Ziwei Cao <zcao85@login-pace-ice-1.pace.gatech.edu>
2022-04-08 00:25:13 -0400

    update kernel function

--- kernel_function/kernel_test.cu
+++ kernel_function/kernel_test.cu
@@ -12,6 +12,8 @@ int main(){
     // In host, initialize a 2d matrix h_x (using a vector with size in numOfData*numOfAttr );
     // parameters:
     float C = 0.1;
+    float slack = 0.1;
+
     // In host:
     int numOfData = 10;
     int numOfAttr = 5;
@@ -36,18 +38,22 @@ int main(){
     float *d_alpha;
     int *d_Iup;
     int *d_Ilow;
+    float *kernel_value;
+    int numOfRowInKernel = 500;
     size_t bytesOfe = numOfData*sizeof(float);
     size_t bytesOfalpha = numOfData*size(float);
     size_t bytesOfIup = numOfData*sizeof(int);
     size_t bytesOfIlow = numOfData*sizeof(int);
+    size_t bytesOfkernel_value = numOfData*numOfRowInKernel*sizeof(float);
     cudaMalloc(&d_x, bytesOfX);    // cudaMalloc(void**, devPtr, size_t, size)
     cudaMalloc(&d_y, bytesOfY);
     cudaMalloc(&d_e, bytesOfe);
     cudaMalloc(&d_alpha, bytesOfalpha);
     cudaMalloc(&d_Iup, bytesOfIup);
     cudaMalloc(&d_Ilow, bytesOfIlow);
+    cudaMalloc(&kernel_value,bytesOfkernel_value); // allocate memory of kernel value in device
     
-        // from host to device
+    // from host to device
     cudaMemcpy(d_x, h_x.data(), bytesOfX, cudaMemcpyHostToDevice);
     cudaMemcpy(d_y, h_y.data(), bytesOfY, cudaMemcpyHostToDevice);
     
@@ -65,10 +71,10 @@ int main(){
         BLOCKS = numOfData/THREADS+1;
     }
 
-    // Initial value
+    // Go to device, Initial value
     smo_kernel_initial<<BLOCKS, THREADS>>(d_x,d_y,d_e, d_alpha,d_Iup, d_Ilow, numOfData,numOfAttr, C);
     
-    // from device to host
+    // Back to host
     cudaMemcpy(h_e.data(), d_e, bytesOfe, cudaMemcpyDeviceToHost);
     cudaMemcpy(h_Ilow.data(),d_Ilow, bytesOfIlow, cudaMemcpyDeviceToHost);
     cudaMemcpy(h_Iup.data(),d_Iup, bytesOfIup, cudaMemcpyDeviceToHost);
@@ -77,6 +83,26 @@ int main(){
     int low=-1;
     int up=-1;
     findSupportVector(h_e, h_Ilow, h_Iup, &low, &up);
+
+    // Loop begin
+    while(1){
+        float bup = h_e[up];
+        float blow = h_e[low];
+        if(blow<=bup+2*slack){
+            break;
+        }
+        // Go to device
+
+        // 1. get kernel value
+
+        // 2. compute alpha, e and Iup Ilow
+
+        // Back to host
+        
+
+    }
+
+    
 
     // free memory
     cudaFree(d_x);
