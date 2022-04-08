9f1551814c1f7f0161c8174df9b537c7d0622f01
Tinglin Hsia <42856623+TinglinHsia980525@users.noreply.github.com>
2022-04-08 02:34:25 -0400

    Update read_csv.c

--- read_csv/read_csv.c
+++ read_csv/read_csv.c
@@ -10,19 +10,31 @@
 #include <string.h>
 
 #define LONGLENGTH 1024
+#define BUFFERSIZE 10
 #define HEAD 1
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    FILE* fp;
+    // Make the Declaration
     char a[1024][2];
-    int line=0;
+    char* line_list,* ptr;
+    char* p;
+    int N;
+    N = BUFFERSIZE;
+    p = 0;
+    //char buffer[BUFFERSIZE], s[BUFFERSIZE];
+    const char* delim = ",";
+    //int line=0;
     int count=0;
     int c, i;
     
-    //fp = fopen("/Users/dinglin/Desktop/Operations Research/ISyE6679 Computational Methods/Final Project/MUSK_testing.csv", "rt"); // rt means reading only
-    fp = fopen("/Users/dinglin/Downloads/CUDA_SVM-main/Dataset/MUSK_original.csv","rt");
+    // Read the File
+    FILE* fp;
+    fp = fopen("/Users/dinglin/Desktop/Operations Research/ISyE6679 Computational Methods/Final Project/MUSK_testing.csv", "rt"); // rt means reading only
+    //fp = fopen("/Users/dinglin/Downloads/CUDA_SVM-main/Dataset/MUSK_original.csv","rt");
     if (fp == NULL) return 2;
+    
+    // Method1: Get all the strings at once, then devide them by strtok
+    // ----- Version 1 ------
     /*
     while(1) {
         i = 0;
@@ -45,14 +57,27 @@ int main(int argc, const char * argv[]) {
              printf("\n");
          }
      }
-     //*/
+     */
     
+    // ----- Version 2 ------
+    /*
+    fseek(fp, 0L, SEEK_SET);
+    line_list = fgets(buffer,BUFFERSIZE,fp);
+    strcpy(s, line_list); // convert pointer to constant
+    printf("s=%s\n",s);
+    line_list = fgets(buffer,BUFFERSIZE,fp);
+    strcpy(s, line_list); // convert pointer to constant
+    printf("s=%s\n",s);
+     */
+    
+    // ----- Version 3 ------
+    /*
     while (fp != EOF && count < LONGLENGTH){
-        /*if (HEAD == 1 && count < 1) {
+        //*if (HEAD == 1 && count < 1) {
             count += 1;
             continue;
         }
-         /*/
+         //*
         fgets(a, sizeof(a), fp);
         char* tmp = strdup(a);
         char* tok;
@@ -65,9 +90,62 @@ int main(int argc, const char * argv[]) {
         printf("%d,1,%c\n", count, a[count][1]);
         count++;
         free(tmp);
+    }*/
+
+    
+    // Method2: Find the number before commas one by one
+    /*
+    for (int k = 0; k < N; k++){
+        //fseek(fp, p, 1);
+        line = fgets(buffer, sizeof(buffer), fp);
+        record = strtok(line, ",");
+        while (record != NULL) {
+            if (strcmp(record, "Ps:") == 0)//当读取到Ps那一行时，不再继续读取
+                return 0;
+            printf("%s ", record);//将读取到的每一个数据打印出来
+            if (p == 10)  //只需读取前9列
+                break;
+            record = strtok(NULL, ",");
+            p++;
+        }
+        printf("\n");
+        p = 0;
+    }
+     */
+    char *line,*record;
+    char buffer[1024];
+    if ((fp = fopen("/Users/dinglin/Desktop/Operations Research/ISyE6679 Computational Methods/Final Project/MUSK_testing.csv", "rt")) != NULL) {
+        fseek(fp, 0, SEEK_SET);  //定位到第二行，每个英文字符大小为1
+        char delims[] = ",";
+        char *result = NULL;
+        int j = 0;
+        while ((line = fgets(buffer, sizeof(buffer), fp))!=NULL) {//当没有读取到文件末尾时循环继续{
+                record = strtok(line, ",");
+                while (record != NULL)//读取每一行的数据
+                {
+                    if (strcmp(record, "Ps:") == 0)//当读取到Ps那一行时，不再继续读取
+                        return 0;
+                    printf("%s \n", record);//将读取到的每一个数据打印出来
+                    if (j == 10)  //只需读取前9列
+                        break;
+                    record = strtok(NULL, ",");
+                    j++;
+                }
+                printf("\n");
+                j = 0;
+     
+            }
+            fclose(fp);
+            fp = NULL;
     }
     
     fclose(fp);
+    
+    
+    
+    printf("Hello, World!\n");
+    return 0;
+}
     
     
     
